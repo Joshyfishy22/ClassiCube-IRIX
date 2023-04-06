b80faf5354effd0cef746a52e116f7524de37865
Joshyfishy22 <jmbastiani1@gmail.com>
2023-04-05 22:18:05 -0500

    optimizations?
    Set Leaves to face cull on IRIX, disabled cave generation for IRIX because the lack of occlusion culling.

--- src/Generator.c
+++ src/Generator.c
@@ -624,7 +624,10 @@ void NotchyGen_Generate(void) {
 
 	NotchyGen_CreateHeightmap();
 	NotchyGen_CreateStrata();
+	//Turns off caves for IRIX performance because of the lack of occlusion culling 
+	#ifndef CC_BUILD_IRIX
 	NotchyGen_CarveCaves();
+	#endif
 	NotchyGen_CarveOreVeins(0.9f, "Carving coal ore", BLOCK_COAL_ORE);
 	NotchyGen_CarveOreVeins(0.7f, "Carving iron ore", BLOCK_IRON_ORE);
 	NotchyGen_CarveOreVeins(0.5f, "Carving gold ore", BLOCK_GOLD_ORE);
